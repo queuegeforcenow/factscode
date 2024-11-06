c4852e3c5731e4a48a1018ce62653cbc457d0f55
queuegeforcenow <queuegeforcenow@gmail.com>
2024-11-06 14:38:38 +0900

    Update authenticate.js

--- .github/api/authenticate.js
+++ .github/api/authenticate.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
         const { mcid, recaptchaResponse } = req.query;
 
         // reCAPTCHAトークンの検証
-        const secretKey = 'YOUR_RECAPTCHA_SECRET_KEY'; // reCAPTCHAのシークレットキーを設定
+        const secretKey = '6LdLoHYqAAAAAPSxJD0MTJBfiq8zdSrbR52UGYYW'; // reCAPTCHAのシークレットキーを設定
         const verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptchaResponse}`;
 
         try {
